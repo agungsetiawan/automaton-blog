6baaa3ea456e04217b71e415590868b4fab64e8c
Agung Setiawan <com.agungsetiawan@gmail.com>
2015-10-21 14:02:23 +0700

    Sessions controller for login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  	@user=User.find_by_username(params[:session][:username])
+
+  	if @user && @user.authenticate(params[:session][:password])
+  	  session[:user_id]=@user.id
+  	  #todo
+  	  #redirect_to dashboard_path
+  	else
+  	  redirect_to login_path, alert: 'Username and or password incorret'
+  	end
+  end
+
+  def destroy
+    session[:user_id]=nil
+    redirect_to login_path
+  end
+
+end
